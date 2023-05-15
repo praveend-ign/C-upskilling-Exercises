e241b367af08d94536cbc3a9e3cd1f97919bea53
Praveen Deshpande (ignitarium) <99164329+praveend-ign@users.noreply.github.com>
2023-05-16 00:41:48 +0530

    gdb coredump exercises

---
+++ topic-7/gdb_Q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+long factorial(int s)
+{
+    if(s==0)
+	    return 1;
+    return (s*factorial(s-1));
+}
+
+int main(void)
+{
+    int num;
+    long result;
+
+    printf("Enter a num:\n");
+    scanf("%d", &num);
+
+    result = factorial(num);
+    printf("factorial of %d = %ld\n", num, result);
+
+    return 0;
+}
